2c333df5c8c380f31272a54882d7ce635dfadb7f
Dimitar Marenoski <91897736+mDimitar@users.noreply.github.com>
2023-09-28 17:26:59 +0200

    added Auto Key Generation In RecivedGoodsBy

--- Models/ReceivedGoodsBy.cs
+++ Models/ReceivedGoodsBy.cs
@@ -1,7 +1,12 @@
-﻿namespace DominionWarehouseAPI.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace DominionWarehouseAPI.Models
 {
     public class ReceivedGoodsBy
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public int UserId { get; set; }
         public User User { get; set; }
